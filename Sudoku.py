88e950bf89068feeb219ba0109d3cb1578a1c75a
Iury Ferreira <1uryf3rr31r4@gmail.com>
2022-09-25 00:28:10 -0300

    Import pacotes, selecao cores e tela do jogo

---
+++ Sudoku.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import pandas as pd
+import random
+import math
+
+# Cores
+preto = (0, 0, 0)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+azul_claro = (200, 200, 255)
+azul = (100, 100, 255)
+branco = (255, 255, 255)
+
+# Tela do Jogo
+window = pg.display.set_mode((1000, 700))
